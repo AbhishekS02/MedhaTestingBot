3a892cc3a1e6dcec9cfc245e9a35903b1749f4be
AbhishekS02 <sinha.ab@hcl.com>
2018-10-22 23:09:08 +0530

    test2

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, Azure!');
+    res.end('Hello, Azure, How are you!');
     
 }).listen(process.env.PORT || 8080);
