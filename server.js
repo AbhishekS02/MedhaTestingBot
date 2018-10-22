a52b17dac4e89b1103cf5d5372eee8d3f0a0a48b
AbhishekS02 <sinha.ab@hcl.com>
2018-10-22 23:50:00 +0530

    Initial bot server

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var http = require('http');
+var restify = require('restify'); 
+var builder = require('botbuilder');  
+// Setup Restify Server 
+var server = restify.createServer(); 
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, Azure, How are you!');
-    
-}).listen(process.env.PORT || 8080);
+server.listen(process.env.port || process.env.PORT || 3978, 
+function () {    
+    console.log('%s listening to %s', server.name, server.url);  
+});  
+// chat connector for communicating with the Bot Framework Service 
+var connector = new builder.ChatConnector({     
+    appId: process.env.MICROSOFT_APP_ID,     
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+// Listen for messages from users  
+server.post('/api/messages', connector.listen()); 
+ 
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:') 
+var bot = new builder.UniversalBot(connector, function (session) {     
+session.send("You said: %s", session.message.text); 
+});
