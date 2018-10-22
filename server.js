2f3687e41a8ab24b0ad85a2455fceb6c94f9cc88
AbhishekS02 <sinha.ab@hcl.com>
2018-10-23 00:33:30 +0530

    Signed-off-by: AbhishekS02 <sinha.ab@hcl.com>

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ function () {
 });  
 // chat connector for communicating with the Bot Framework Service 
 var connector = new builder.ChatConnector({     
-    appId: process.env.MICROSOFT_APP_ID,     
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "D8005D4A-9DB2-4106-A320-D4C0551ACC20",     
+    appPassword: "8306DDEC-790D-4F8E-9950-C0850169AB3D"
 });
 // Listen for messages from users  
 server.post('/api/messages', connector.listen()); 
